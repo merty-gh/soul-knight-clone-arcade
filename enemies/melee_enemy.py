224a231728d7e510403a4aec130bcb8d506fe900
merty-gh <yanellefn@mail.ru>
2026-01-29 16:51:03 +0300

    Delete enemies directory

--- enemies/melee_enemy.py
+++
@@ -1,7 +0,0 @@
-from enemies.base_enemy import BaseEnemy
-
-class MeleeEnemy(BaseEnemy):
-    def __init__(self, x, y, player_ref):
-        # Используем спрайт зомби
-        super().__init__(":resources:images/animated_characters/zombie/zombie_idle.png",
-                         x, y, player_ref)
